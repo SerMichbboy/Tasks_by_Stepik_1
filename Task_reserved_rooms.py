f5f5f88d4b07ce1dba03fb5b0157055ff4032ca9
Sergo <Sergofiles-16@mail.ru>
2023-04-25 12:37:05 +0300

    Chnges is good

--- Task_reserved_rooms.py
+++ Task_reserved_rooms.py
@@ -2,7 +2,9 @@ import datetime
 
 def is_available_date(booked_dates, date_for_booking):
 
-
 dates = ['04.11.2021', '05.11.2021-09.11.2021']
 some_date = '01.11.2021'
 print(is_available_date(dates, some_date))
+
+
+#Не закончена
